ea0ae25631498fff8d7e4598e383388283d50fcf
nekomeo <linh.tu545@gmail.com>
2017-09-05 23:07:21 -0700

    Add Graph view with tap gesture

---
+++ Flo/GraphView.swift
@@ -0,0 +1,21 @@
+//
+//  GraphView.swift
+//  Flo
+//
+//  Created by Elle Ti on 2017-09-05.
+//  Copyright © 2017 Elle Ti. All rights reserved.
+//
+
+import UIKit
+
+class GraphView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
--- Flo/ViewController.swift
+++ Flo/ViewController.swift
@@ -10,8 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    var isGraphViewShowing = false
+    
     @IBOutlet weak var counterView: CounterView!
     @IBOutlet weak var counterLabel: UILabel!
+    @IBOutlet weak var containerView: UIView!
+    @IBOutlet weak var graphView: GraphView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -35,6 +39,24 @@ class ViewController: UIViewController {
             }
         }
         counterLabel.text = String(counterView.counter)
+        
+        if isGraphViewShowing {
+            counterViewTap(gesture: nil)
+        }
+    }
+    
+    
+    @IBAction func counterViewTap(gesture:UITapGestureRecognizer?) {
+        if (isGraphViewShowing) {
+            // Hide the graph
+            UIView.transition(from: graphView, to: counterView, duration: 1.0, options: [.transitionFlipFromLeft, .showHideTransitionViews], completion: nil) //UIViewAnimationOptions.TransitionFlipFromLeft
+//                | UIViewAnimationOptions.ShowHideTransitionViews, completion: nil)
+        } else {
+            // Show graph
+            UIView.transition(from: counterView, to: graphView, duration: 1.0, options: [.transitionFlipFromRight, .showHideTransitionViews], completion: nil)//UIViewAnimationOptions.TransitionFlipFromRight
+//                | UIViewAnimationOptions.ShowHideTransitionViews, completion: nil)
+        }
+        isGraphViewShowing = !isGraphViewShowing
     }
 }
 
