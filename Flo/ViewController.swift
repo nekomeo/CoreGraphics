98966b2fca8930bfda0c18f28ed107a75cff6acd
nekomeo <linh.tu545@gmail.com>
2017-09-05 19:31:31 -0700

    Complete first tutorial

---
+++ Flo/ViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  Flo
+//
+//  Created by Elle Ti on 2017-09-05.
+//  Copyright © 2017 Elle Ti. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var counterView: CounterView!
+    @IBOutlet weak var counterLabel: UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        counterLabel.text = String(counterView.counter)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+    @IBAction func btnPushButton(button: PushButtonView) {
+        if button.isAddButton {
+            counterView.counter += 1
+        } else {
+            if counterView.counter > 0 {
+                counterView.counter -= 1
+            }
+        }
+        counterLabel.text = String(counterView.counter)
+    }
+}
+
